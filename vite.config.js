6116ea340c7aa67a11d9226303ee25a23c6de8a2
Mohamed Hafidi <saimodev@gmail.com>
2023-03-19 14:48:26 +0000

    feat: install & setup Coreui template

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/style.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
