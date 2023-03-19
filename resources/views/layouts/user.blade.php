191897d6ec68df1267e28fe1c1d38cde571af245
Mohamed Hafidi <saimodev@gmail.com>
2023-03-19 14:50:44 +0000

    feat: Add views layouts

---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+</head>
+<body>
+
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    
+    <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
+    @vite(['resources/sass/app.scss', 'resources/sass/style.scss'])
+</body>
+</html>
