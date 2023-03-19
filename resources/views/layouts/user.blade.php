a2c84d681626b8cedc59bfe726147133a87be2ff
Mohamed Hafidi <saimodev@gmail.com>
2023-03-19 15:16:48 +0000

    refact: update user layout view

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -9,15 +9,44 @@
 </head>
 <body>
 
-    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+    <nav class="navbar navbar-expand-lg bg-light mb-4">
         <div class="container">
-            <div class="row justify-content-center">
-                @yield('content')
+            <a class="navbar-brand" href="{{ route('home') }}">
+                Navbar
+            </a>
+            <button class="navbar-toggler" type="button" data-coreui-toggle="collapse" data-coreui-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-coreui-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+    
+    <div class="container">
+        @yield('content')
     </div>
     
     <script src="{{ asset('js/coreui.bundle.min.js') }}"></script>
     @vite(['resources/sass/app.scss', 'resources/sass/style.scss'])
+
+    @stack('scripts')
 </body>
 </html>
