652e46fb14853d12e7d7928a4a323b08c7af6a6f
Mohamed Hafidi <saimodev@gmail.com>
2023-03-19 14:53:04 +0000

    feat: Add admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/dashboard', [DashboardController\Index::class, 'index'])->name('admin.dashboard');
